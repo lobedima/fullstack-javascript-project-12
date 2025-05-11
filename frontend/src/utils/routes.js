7a6436b4141595b21a2050113efb025ac9812880
Dmitriy Lyubenko <lobedima@gmail.com>
2025-05-11 06:05:52 +0300

    Fix ESLint errors

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -2,25 +2,25 @@ export const pages = {
   signup: () => '/signup',
   login: () => '/login',
   root: () => '/',
-}
+};
 
-const verApi = process.env.API_VER || '/api/v1'
+const verApi = process.env.API_VER || '/api/v1';
 
 export const users = {
   login: () => `${verApi}/login`,
   signup: () => `${verApi}/signup`,
-}
+};
 
 export const channels = {
   getAll: () => `${verApi}/channels`,
   post: () => `${verApi}/channels`,
   patch: (channelId) => `${verApi}/channels/${channelId}`,
   delete: (channelId) => `${verApi}/channels/${channelId}`,
-}
+};
 
 export const messages = {
   getAll: () => `${verApi}/messages`,
   post: () => `${verApi}/messages`,
   patch: (messageId) => `${verApi}/messages/${messageId}`,
   delete: (messageId) => `${verApi}/messages/${messageId}`,
-}
+};
